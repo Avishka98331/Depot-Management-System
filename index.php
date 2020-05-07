d6c04e6c78c9c1b12b319795f20cad32bc6f4db7
Avishka98331 <63835510+Avishka98331@users.noreply.github.com>
2020-05-07 17:16:48 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -1,18 +1,28 @@
 <?php
-  require "header.php";
+  session_start();
+  /*include 'includes/class-autoload.inc.php';*/
 ?>
-    <main>
-      <?php
-        if(isset($_SESSION['userUid'])) {
-          echo '<p style="text-align: center;">You are logged in</p>';
-        }
-        else{
-          echo '<p style="text-align: center;">You are logged out</p>';
-        }
-      ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="description" content="Cashier workplace.">
+    <meta name="viewport" content="width-device-width, initial-scaled=1">
+    <title>Cashier section</title>
+  </head>
+  <body>
+    <header>
+      <nav>
+        <ul>
+          <li><a href="view\tbinitiate.view.php">Initiate Ticket Book</a></li>
+          <li><a href="view\tbclose.view.php">Close Ticket Book</a></li>
+          <li><a href="view\createcomplain.view.php">Create Complin</a></li>
+          <li><a href="view\createpaysheet.view.php">Create paysheet</a></li>
+        </ul>
+      </nav>
+    </header>
+  </body>
+  <main>
 
-
-    </main>
-<?php
-  require "footer.php";
-?>
+  </main>
+</html>
